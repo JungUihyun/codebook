384fd269d5595c396cb7525aa25ff451bf58fa5e
twins200202 <twins2002002@gmail.com>
2020-01-16 16:17:33 +0900

    Thursday Practice

--- src/main/java/net/gondr/dao/BoardDAOImpl.java
+++ src/main/java/net/gondr/dao/BoardDAOImpl.java
@@ -2,9 +2,11 @@ package net.gondr.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import net.gondr.domain.BoardVO;
 
+@Repository
 public class BoardDAOImpl implements BoardDAO {
 	@Autowired
 	private SqlSession session;
